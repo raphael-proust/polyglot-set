7de860cdac08e165e494b34df68d7b5874839214
Raphaël Proust <code@bnwr.net>
2022-02-27 09:46:47 +0100

    Improve rust implementation

--- rust/set/src/main.rs
+++ rust/set/src/main.rs
@@ -1,66 +1,79 @@
-#![allow(dead_code)]
-use std::fmt;
+pub mod card_prop {
+    use std::fmt;
 
-fn triplet<T: std::cmp::PartialEq> (pa: &T, pb: &T, pc: &T) -> bool {
-    (pa == pb && pb == pc) || (pa != pb && pb != pc && pa != pc)
-}
+    pub fn triplet<T: std::cmp::PartialEq> (pa: &T, pb: &T, pc: &T) -> bool {
+        (pa == pb && pb == pc) || (pa != pb && pb != pc && pa != pc)
+    }
 
-macro_rules! cardprop {
+    macro_rules! makeprop {
 
-    ($name: ident, $p0: ident, $p1: ident, $p2: ident) => {
+        ($name: ident, $p0: ident, $p1: ident, $p2: ident) => {
 
-        #[derive(PartialEq, Copy, Clone)]
-        enum $name { $p0, $p1, $p2 }
-        impl fmt::Display for $name {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match self {
-                    $name::$p0 => write!(f, stringify!($p0)),
-                    $name::$p1 => write!(f, stringify!($p1)),
-                    $name::$p2 => write!(f, stringify!($p2)),
+            #[derive(PartialEq, Clone)]
+            pub enum $name { $p0, $p1, $p2 }
+            impl fmt::Display for $name {
+                fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                    match self {
+                        $name::$p0 => write!(f, stringify!($p0)),
+                        $name::$p1 => write!(f, stringify!($p1)),
+                        $name::$p2 => write!(f, stringify!($p2)),
+                    }
                 }
             }
-        }
 
-    };
+        };
 
-}
-
-cardprop!(Shape, Wave, Pill, Diamond);
-cardprop!(Fill, Empty, Partial, Full);
-cardprop!(Colour, Red, Green, Purple);
-cardprop!(Count, One, Two, Three);
-
-struct Card {
-    shape: Shape,
-    fill: Fill,
-    colour: Colour,
-    count: Count,
-}
-
-fn triplet_card (ca : &Card, cb: &Card, cc: &Card) -> bool {
-    triplet (&ca.shape, &cb.shape, &cc.shape)
-    && triplet (&ca.fill, &cb.fill, &cc.fill)
-    && triplet (&ca.colour, &cb.colour, &cc.colour)
-    && triplet (&ca.count, &cb.count, &cc.count)
-}
-
-impl fmt::Display for Card {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}:{}:{}:{}", self.shape, self.fill, self.colour, self.count)
     }
+
+    makeprop!(Shape, Wave, Pill, Diamond);
+    makeprop!(Fill, Empty, Partial, Full);
+    makeprop!(Colour, Red, Green, Purple);
+    makeprop!(Count, One, Two, Three);
+
+}
+
+pub mod card {
+    use std::fmt;
+    use crate::card_prop;
+
+    pub struct Card {
+        shape: card_prop::Shape,
+        fill: card_prop::Fill,
+        colour: card_prop::Colour,
+        count: card_prop::Count,
+    }
+    impl Card {
+        pub fn new(shape: card_prop::Shape, fill: card_prop::Fill, colour: card_prop::Colour, count: card_prop::Count) -> Card {
+            Card { shape, fill, colour, count, }
+        }
+    }
+
+    pub fn triplet (ca : &Card, cb: &Card, cc: &Card) -> bool {
+        card_prop::triplet (&ca.shape, &cb.shape, &cc.shape)
+            && card_prop::triplet (&ca.fill, &cb.fill, &cc.fill)
+            && card_prop::triplet (&ca.colour, &cb.colour, &cc.colour)
+            && card_prop::triplet (&ca.count, &cb.count, &cc.count)
+    }
+
+    impl fmt::Display for Card {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}:{}:{}:{}", self.shape, self.fill, self.colour, self.count)
+        }
+    }
+
 }
 
 fn main() {
-    let shapes: [Shape; 3] = [Shape::Wave, Shape::Pill, Shape::Diamond ];
-    let fills: [Fill; 3] = [Fill::Empty, Fill::Partial, Fill::Full ];
-    let colours: [Colour; 3] = [Colour::Red, Colour::Green, Colour::Purple ];
-    let counts: [Count; 3] = [Count::One, Count::Two, Count::Three ];
-    let mut all_cards: Vec<Card> = vec![];
+    let shapes: [card_prop::Shape; 3] = [card_prop::Shape::Wave, card_prop::Shape::Pill, card_prop::Shape::Diamond ];
+    let fills: [card_prop::Fill; 3] = [card_prop::Fill::Empty, card_prop::Fill::Partial, card_prop::Fill::Full ];
+    let colours: [card_prop::Colour; 3] = [card_prop::Colour::Red, card_prop::Colour::Green, card_prop::Colour::Purple ];
+    let counts: [card_prop::Count; 3] = [card_prop::Count::One, card_prop::Count::Two, card_prop::Count::Three ];
+    let mut all_cards: Vec<card::Card> = vec![];
     for shape in shapes.iter() {
         for fill in fills.iter() {
             for colour in colours.iter() {
                 for count in counts.iter() {
-                    all_cards.push(Card { shape: *shape, fill: *fill, colour: *colour, count: *count })
+                    all_cards.push(card::Card::new( shape.clone(), fill.clone(), colour.clone(), count.clone() ))
                 }
             }
         }
@@ -69,7 +82,7 @@ fn main() {
     for (ai, ca) in all_cards.iter().enumerate() {
         for (bi, cb) in all_cards[ai+1..].iter().enumerate() {
             for (_ci, cc) in all_cards[bi+1..].iter().enumerate() {
-                if triplet_card(&ca, &cb, &cc) {
+                if card::triplet(&ca, &cb, &cc) {
                     println!("Triplet found! {}  {}  {}", ca, cb, cc)
                 }
             }
